af5ca90e269b17f8127e3f231869ab9953d87a1d
Victor323232 <victorhugoabc59@gmail.com>
2023-02-17 15:54:53 -0300

    Login pronto

---
+++ keep-svelte/src/pages/auth/Login.svelte
@@ -0,0 +1,41 @@
+<main>
+    <div class="container my-5">
+        <div class="row">
+            <div
+                class="col-xl-6 col-lg-6 col-md-8 col-sm-12 offset-xl-3 offset-lg-3 offset-md-2 offset-sm-12"
+            >
+                <div class="card">
+                    <div class="card-header">
+                        <h3>Fazer login</h3>
+                    </div>
+                    <div class="card-body">
+                        <form action="" >
+                           
+                            <div class="mb-3">
+                                <label for=""><b>Entrar com Email</b></label>
+                                <input
+                                    type="email"
+                                
+                                    class="form-control form-control-lg"
+                                />
+                            </div>
+                            <div class="mb-3">
+                                <label for=""><b>Entrar com Senha</b></label>
+                                <input
+                                    type="password"
+                
+                                    class="form-control form-control-lg"
+                                />
+                            </div>
+                            <div class="mb-3">
+                                <button class="btn btn-success" type="submit"
+                                    >Login</button
+                                >
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>

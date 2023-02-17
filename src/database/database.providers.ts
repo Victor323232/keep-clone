8c20bb42575e605cf7ff700d4e00d484cb42d17b
Victor323232 <victorhugoabc59@gmail.com>
2023-02-17 04:52:44 -0300

    Banco de dados

---
+++ src/database/database.providers.ts
@@ -0,0 +1,23 @@
+import { DataSource } from 'typeorm';
+
+export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async () => {
+      const dataSource = new DataSource({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5433,
+        username: 'postgres',
+        password: 'sasuke123',
+        database: 'keepclone',
+        entities: [
+            __dirname + '/../**/*.entity{.ts,.js}',
+        ],
+        synchronize: true,
+      });
+
+      return dataSource.initialize();
+    },
+  },
+];

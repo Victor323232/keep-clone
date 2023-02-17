9ed748627529ce4661218f1cdab84de65290e319
Victor323232 <victorhugoabc59@gmail.com>
2023-02-17 15:32:10 -0300

    criacao navegao do header

--- keep-svelte/src/App.svelte
+++ keep-svelte/src/App.svelte
@@ -1,28 +1,21 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
+  import  Router  from "svelte-spa-router";
+    import Login from "./pages/auth/Login.svelte";
+    import Signup from "./pages/auth/Signup.svelte";
+    import CreatePost from "./pages/post/CreatePost.svelte";
+    import Home from "./pages/post/Home.svelte";
+    import UpdatePost from "./pages/post/UpdatePost.svelte";
+  let routes ={
+    "/":Home,
+    "/login":Login,
+    "/signup":Signup,
+    "/create-post":CreatePost,
+    "/update-post":UpdatePost,
+  }
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+<Router {routes}/>
 </main>
 
 <style>

78d0f137f3803024205d0f78917388edc930b6f3
Victor323232 <victorhugoabc59@gmail.com>
2023-02-17 15:04:49 -0300

    redefinido

--- keep-svelte/src/services/store.js
+++
@@ -1,4 +0,0 @@
-import { writable } from 'svelte/store';
-
-// Criar uma nova store chamada tasks iniciada com uma array vazia. A store é usada para compartilhar dados entre os componentes Svelte.
-export const tasks = writable([]);

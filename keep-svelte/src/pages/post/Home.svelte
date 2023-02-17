c2fcecafbdda16660c84687543606a015c8e03f8
Victor323232 <victorhugoabc59@gmail.com>
2023-02-17 16:17:14 -0300

    Pagina home com a lista de tarefas

---
+++ keep-svelte/src/pages/post/Home.svelte
@@ -0,0 +1,28 @@
+<main>
+    <div class="container my-5">
+        <div class="row">
+            <div
+                class="col-xl-6 col-lg-6 col-md-8 col-sm-12 offset-xl-3 offset-lg-3 offset-md-2 offset-sm-12">
+           
+                <div class="card">
+                    <div class="card-body">
+                        <h3>OI</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Title</th>
+                                <th>Content</th>
+                                <th>Price</th>
+                                <th>UserName</th>
+                                <th>Edit</th>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
+                        
+    </div>
+</main>
